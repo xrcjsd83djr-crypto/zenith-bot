1d257a0490b78115fefb174eff26e2edb08de270
xrcjsd83djr-crypto <xrcjsd83djr@gmail.com>
2026-05-08 19:19:10 -0400

    feat: Zenith Discord bot - full implementation

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,21 +1,39 @@
 import "dotenv/config";
 import { REST, Routes } from "discord.js";
-import { commands } from "./commands/index.js";
+import { config } from "./lib/config.js";
 
-const token = process.env.DISCORD_TOKEN!;
-const clientId = process.env.DISCORD_CLIENT_ID!;
+import * as strikeCommand from "./commands/strike.js";
+import * as staffCommand from "./commands/staff.js";
+import * as loaCommand from "./commands/loa.js";
+import * as rankCommand from "./commands/rank.js";
+import * as activityCommand from "./commands/activity.js";
+import * as configCommand from "./commands/config.js";
+import * as helpCommand from "./commands/help.js";
 
-const rest = new REST({ version: "10" }).setToken(token);
+const commands = [
+  strikeCommand,
+  staffCommand,
+  loaCommand,
+  rankCommand,
+  activityCommand,
+  configCommand,
+  helpCommand,
+].map(c => c.data.toJSON());
 
-const commandData = commands.map((c) => c.data.toJSON());
+const rest = new REST().setToken(config.token);
 
-(async () => {
+async function deploy() {
+  console.log(`[Zenith] Deploying ${commands.length} slash commands globally...`);
   try {
-    console.log(`[Zenith] Deploying ${commandData.length} slash commands...`);
-    await rest.put(Routes.applicationCommands(clientId), { body: commandData });
-    console.log("[Zenith] Commands deployed successfully.");
+    const data = await rest.put(
+      Routes.applicationCommands(config.clientId),
+      { body: commands },
+    ) as any[];
+    console.log(`[Zenith] Successfully deployed ${data.length} commands.`);
   } catch (error) {
     console.error("[Zenith] Failed to deploy commands:", error);
     process.exit(1);
   }
-})();
+}
+
+deploy();
